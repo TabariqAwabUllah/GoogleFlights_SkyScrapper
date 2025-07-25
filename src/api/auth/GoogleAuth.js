85289234e2238e35ccfe5b962925c5e9c13dd890
Tabariqawabullah <Tabariqawabullah@gmail.com>
2025-07-25 23:31:24 +0500

    Google Sign in

--- src/api/auth/GoogleAuth.js
+++ src/api/auth/GoogleAuth.js
@@ -6,7 +6,7 @@ GoogleSignin.configure({
     webClientId: '166956293591-0uhqra54o83a93vdfqdv204r2eleb7ii.apps.googleusercontent.com',
 })
 
-console.log("Google Auth file");
+// console.log("Google Auth file");
 
 
 export async function onGoogleButtonPress() {
@@ -14,20 +14,22 @@ export async function onGoogleButtonPress() {
   console.log("try 1");
 
   try {
-    console.log("try 1");
+    console.log("try 2");
     
     const see = await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true})
     console.log("See",see);
     
 
     const signIn = await GoogleSignin.signIn()
+    console.log("Sign IN", signIn);
+    
 
     const idToken = signIn.idToken
     if(!idToken){
       throw new Error("Token not found of G sign in")
     }
     const googleCredentials = GoogleAuthProvider.credentials(idToken)
-    const authResult = await signInWithCredential(getAuth(), googleCredential);
+    const authResult = await signInWithCredential(getAuth(), googleCredentials);
     console.log('Firebase Auth Result:', authResult);
 
     return authResult;
